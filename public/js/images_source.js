135fdf3cc7efc57c74e66ab44286f962792ebdac
Yue Cheng <chengyue@utexas.edu>
2021-04-17 18:53:07 -0500

    added source information

---
+++ public/js/images_source.js
@@ -0,0 +1,9 @@
+var sources = {
+    1: "https://en.wikipedia.org/wiki/The_Hippopotamus_and_Crocodile_Hunt",
+    2: "https://en.wikipedia.org/wiki/The_Kiss_(Klimt)",
+    3: "https://whitney.org/collection/works/1542",
+    4: "https://new.artsmia.org/stories/secrets-of-the-veiled-lady-the-passion-and-politics-behind-mias-marble-masterpiece",
+    5: "https://victorianweb.org/art/design/dresser/11.html",
+    6: "https://async.art/art/layer/0xb6dae651468e9593e4581705a09c10a76ac1e0c8-1"
+
+}
